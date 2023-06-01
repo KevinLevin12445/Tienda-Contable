18e704595175dd761e1337b671773bb5170d0e73
KevinLevin12445 <127271114+KevinLevin12445@users.noreply.github.com>
2023-05-31 22:05:53 -0500

    Add files via upload

--- Login-3.5/Login 3.5/sources/addtocart.js
+++ Login-3.5/Login 3.5/sources/addtocart.js
@@ -3,17 +3,20 @@ const product = [
 		"id": 1,
 		"title": "Contabilidad Mensual para Empresas",
 		"price":  6009,
+        "info":"Detalles: Lorem impsum",
 		"image": "https://cdn.shopify.com/s/files/1/0549/3217/7072/products/contabilidad-persona-moral_large.jpg?v=1622761732"
 	},
 	{
 		"id": 2,
 		"title": "Contabilidad Mensual para Personas Físicas",
+        "info":"Detalles: Lorem impsum",
 		"price": 4013,
 		"image": "https://cdn.shopify.com/s/files/1/0549/3217/7072/products/contabilidad-PF-otros-actividad-empresarial_large.jpg?v=1622761869"
 	},
 	{
 		"id": 3,
 		"title": "Contabilidad Mensual para Hosts Airbnb",
+        "info":"Detalles: Lorem impsum",
 		"price": 1560,
 		"image": "https://cdn.shopify.com/s/files/1/0549/3217/7072/products/Contabilidad-Host-Airbnb_large.jpg?v=1621379474"
 	}
@@ -21,21 +24,21 @@ const product = [
 const categories = [...new Set(product.map((item)=>
     {return item}))]
     let i=0;
-document.getElementById('root').innerHTML = categories.map((item)=>
-{
-    var {image, title, price} = item;
-    return(
-        `<div class='box'>
+    document.getElementById('root').innerHTML = categories.map((item) => {
+        var { image, title, price, info } = item;
+        return (
+          `<div class='box'>
             <div class='img-box'>
-                <img class='images' src=${image}></img>
+              <img class='images' src=${image}></img>
             </div>
-        <div class='bottom'>
-        <p>${title}</p>
-        <h2>$ ${price}.00</h2>`+
-        "<button onclick='addtocart("+(i++)+")'>Add to cart</button>"+
-        `</div>
-        </div>`
-    )
+            <div class='bottom'>
+              <p>${title}</p>
+              <p>${info}</p> <!-- Agregar esta línea -->
+              <h2>$ ${price}.00</h2>
+              <button onclick='addtocart(${i++})'>Add to cart</button>
+            </div>
+          </div>`
+        );
 }).join('')
 
 var cart =[];
@@ -63,42 +66,23 @@ function displaycart(){
             total=total+price;
             document.getElementById("total").innerHTML = "$ "+total+".00";
 			return (
-				`<div class='cart-item'>
-				  <div class='row-img'>
-					<img class='rowimg' src=${image}>
-				  </div>
-				  <a href="../paginas/chatIA.html" class="chat-icon"><i class='fa-solid fa-comment-dots'></i></a> <!-- Chat icon with link -->
-				  <i class='fa-solid fa-trash' onclick='delElement(${j++})'></i>
-				  <p style='font-size: 12px;'>${title}</p>
-				  <h2 style='font-size: 15px;'>$ ${price}.00</h2>
-				</div>`
-			  );
+                `<div class='cart-item'>
+                  <div class='row-img'>
+                    <img class='rowimg' src=${image}>
+                  </div>
+                  <a href='../paginas/chatIA.html'>
+                    <i class='fas fa-shopping-cart'></i> <!-- Icono del carrito de compras -->
+                  </a>
+                  <i class='fa-solid fa-trash' onclick='delElement(${j++})'></i>
+                  <p style='font-size: 19px;'>${title}</p>
+                  <h2 style='font-size: 19px;'>$ ${price}.00</h2>
+                </div>`
+              );
+              
+                    
         }).join('');
     }
-
-    
 }
 
-// Función para procesar el mensaje del usuario y generar la respuesta del chatbot
-function procesarMensaje(mensaje) {
-	mensaje = mensaje.toLowerCase(); // Convertir el mensaje a minúsculas para facilitar el procesamiento
-  
-	// Comprobar diferentes casos y generar la respuesta correspondiente
-	if (mensaje.includes('hola') || mensaje.includes('saludos')) {
-	  return '¡Hola! ¿Cómo puedo ayudarte?';
-	} else if (mensaje.includes('cómo estás') || mensaje.includes('qué tal')) {
-	  return 'Estoy programado para responder tus preguntas. ¿En qué puedo ayudarte hoy?';
-	} else if (mensaje.includes('métodos de pago') || mensaje.includes('formas de pago')) {
-	  return 'Aceptamos pagos a través de Daviplata y Nequi. ¿Necesitas más información sobre los pagos?';
-	} else {
-	  return 'Lo siento, no entiendo tu pregunta. ¿Podrías ser más específico?';
-	}
-  }
-  
-  // Ejemplo de uso
-  console.log(procesarMensaje('Hola')); // Salida: ¡Hola! ¿Cómo puedo ayudarte?
-  console.log(procesarMensaje('¿Cómo estás?')); // Salida: Estoy programado para responder tus preguntas. ¿En qué puedo ayudarte hoy?
-  console.log(procesarMensaje('Cuáles son los métodos de pago?')); // Salida: Aceptamos pagos a través de Daviplata y Nequi. ¿Necesitas más información sobre los pagos?
-  console.log(procesarMensaje('¿Qué servicios ofrecen?')); // Salida: Lo siento, no entiendo tu pregunta. ¿Podrías ser más específico?
-  
+      
 
